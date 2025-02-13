f477375c460bfe9bc0c665993c71d9f0e220de02
Patryk <org@prz.edu.pl>
2025-02-14 00:39:17 +0100

    new tasks

---
+++ files/beginner/module_1/lesson_2/tasks/1.3.2.py
@@ -0,0 +1,16 @@
+# Zadanie: 1.3.2.py
+
+def obliczPole(szerokosc, wysokosc):
+    return szerokosc * wysokosc
+
+
+print("Podaj szerokość:")
+szer = int(input())
+
+print("Podaj wysokość:")
+wys = int(input())
+
+pole = obliczPole(szer, wys)
+obwod = 2 * (szer + wys)  # Dodana obliczeniowa obwodu
+
+print("Pole:", pole, "Obwód:", obwod)
---
+++ files/beginner/module_1/lesson_2/tasks/1.3.4.py
@@ -0,0 +1,8 @@
+# Zadanie: 1.3.4.py
+
+import sys
+
+if sys.version_info < (3, 9):
+    print("Stara wersja")
+else:
+    print("Nowa wersja")
