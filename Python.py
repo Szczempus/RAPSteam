f788ad936f529cf3fae839dfe216557ad260ab41
Patryk <org@prz.edu.pl>
2025-02-14 14:31:01 +0100

    imgs

---
+++ Python.py
@@ -0,0 +1,5 @@
+def witaj(imie):
+    
+print("Cześć,", imie, "!")
+
+witaj("Ania")
---
+++ calc.py
@@ -0,0 +1,12 @@
+# Plik: calc.py
+
+def dodaj(a, b):
+    return a + b
+
+
+def odejmij(a, b):
+    return a - b
+
+
+dodaj(5, 3)  # Wynik: 8
+odejmij(10, 4)  # Wynik: 6
