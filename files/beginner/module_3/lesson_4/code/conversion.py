849aa90813c97e4c32a4b5e53ba78795ac3d6f2a
Patryk <org@prz.edu.pl>
2025-02-15 21:32:51 +0100

    tasks

---
+++ files/beginner/module_3/lesson_4/code/conversion.py
@@ -0,0 +1,13 @@
+# Plik: conversion.py
+
+x = "100"
+print(int(x), type(int(x)))  # Konwersja stringa na int
+
+y = 42.5
+print(str(y), type(str(y)))  # Konwersja float na string
+
+z = 10
+print(float(z), type(float(z)))  # Konwersja int na float
+
+lista = [1, 2, 3]
+print(tuple(lista), type(tuple(lista)))  # Konwersja listy na krotkę
