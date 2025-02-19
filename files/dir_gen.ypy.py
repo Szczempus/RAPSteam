82148b4624cf7df48045fa4c0c7bc0ecddf8b12b
Patryk <org@prz.edu.pl>
2025-02-19 20:50:34 +0100

    imgs

--- files/dir_gen.ypy.py
+++ files/dir_gen.ypy.py
@@ -1,7 +1,7 @@
 # Generate Python files from 3_6_1.py to 3_6_20.py
 
 for i in range(1, 21):
-    filename = f"beginner/module_4/summary/tasks/4_5_{i}.py"
+    filename = f"beginner/module_6/summary/tasks/6_6_{i}.py"
     with open(filename, "w") as file:
         file.write("# Auto-generated Python script\n\n")
 
