39120930272d69a9db25e00dd38145f7c545428a
Patryk <org@prz.edu.pl>
2025-02-20 21:46:27 +0100

    make 2.2.x

---
+++ files/beginner/module_2/lesson_2/tasks/2_2_1.py
@@ -0,0 +1,6 @@
+def policz_sume(a, b):
+    result = a + b
+    return result
+
+
+print(policz_sume(5, 10))
---
+++ files/beginner/module_2/lesson_2/tasks/2_2_2.py
@@ -0,0 +1,4 @@
+for i in range(3):
+    print("Pętla zewnętrzna:", i)
+    for j in range(2):
+        print("   Pętla wewnętrzna:", j)
