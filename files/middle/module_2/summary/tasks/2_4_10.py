a24d784f5276d74c260c96249ca3875e6717cd78
Szczempus <lolkikpop@wp.pl>
2025-03-25 19:31:59 +0100

    zmieniono 2_4_10/11/16/20

--- files/middle/module_2/summary/tasks/2_4_10.py
+++ files/middle/module_2/summary/tasks/2_4_10.py
@@ -1,4 +1,13 @@
-# Input: 4 8 12
+# Input: 4, 8, 12
 
-numbers = list(map(float, input("Podaj trzy liczby: ").split()))
-print(f"Średnia: {sum(numbers) / len(numbers)}")
+first = int(input("Podaj pierwszą liczbę: "))
+second = int(input("Podaj drugą liczbę: "))
+third = int(input("Podaj trzecią liczbę: "))
+
+tablica_liczb = []
+
+tablica_liczb.append(first)
+tablica_liczb.append(second)    
+tablica_liczb.append(third)
+
+print(f"Średnia: {sum(tablica_liczb)/ len(tablica_liczb)}")
--- files/middle/module_2/summary/tasks/2_4_20.py
+++ files/middle/module_2/summary/tasks/2_4_20.py
@@ -1,6 +1,5 @@
-# Input: Jabłko 3 Banan 5 Pomarańcza 4
 
-items = input("Podaj trzy produkty i ich ceny \n(np. Jabłko 3 Banan 5 Pomarańcza 4): ").split()
-product_dict = {items[i]: int(items[i + 1]) for i in range(0, len(items), 2)}
-for product, price in product_dict.items():
-    print(f"{product}: {price} zł")
+uczen = ("Marek", 13, ["matematyka", "fizyka", "informatyka"])
+print(f"Przed zmianami: {uczen}")
+uczen[2][2] = "historia"
+print(f"Po zmianach: {uczen}")
