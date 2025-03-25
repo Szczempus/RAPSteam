6e3eefac9f02808db1c06f891fa10797cd2824f1
Szczempus <lolkikpop@wp.pl>
2025-03-25 18:50:56 +0100

    zmiana zadania 1_13_11

--- files/middle/module_1/summary/tasks/1_13_11.py
+++ files/middle/module_1/summary/tasks/1_13_11.py
@@ -1,3 +1,17 @@
-wiek = 30  # int
-wiek = str(wiek)  # zmiana na str
-print(wiek, type(wiek))
+# Input: 4, 5, 3, 2
+
+first = int(input("Podaj pierwszą liczbę: "))
+second = int(input("Podaj drugą liczbę: "))
+third = int(input("Podaj trzecią liczbę: "))
+fourth = int(input("Podaj czwartą liczbę: "))
+
+suma_z_liczb = first + second + third + fourth
+
+print(f"Typ zmiennej przed dzieleniem{type(suma_z_liczb)}")
+
+suma_z_liczb = suma_z_liczb / 4
+
+print(f"Typ zmiennej po dzieleniu{type(suma_z_liczb)}")
+
+print(f"Średnia arytmetyczna z podanych liczb to: {suma_z_liczb}")
+
