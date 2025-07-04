d7d7f8c347de7d0e263e115146d9a6ad05f0662e
LgGram <org@prz.edu.pl>
2025-07-04 20:34:34 +0200

    prior

--- calc.py
+++ calc.py
@@ -1,12 +1,4 @@
 # Plik: calc.py
 
-def dodaj(a, b):
-    return a + b
-
-
-def odejmij(a, b):
-    return a - b
-
-
-dodaj(5, 3)  # Wynik: 8
-odejmij(10, 4)  # Wynik: 6
+print(5+3)
+print(10-4)
---
+++ files/beginner/module_1/lesson_3/code/prior.py
@@ -0,0 +1,8 @@
+# Plik: prior.py
+
+# Najpierw mnożenie: 3*4 = 12, potem dodawanie: 2+12 = 14
+print("Wynik 1:", 2 + 3 * 4)
+
+# Najpierw nawias: 2+3 = 5, potem mnożenie: 5*4 = 20
+print("Wynik 2:", (2 + 3) * 4)
+
